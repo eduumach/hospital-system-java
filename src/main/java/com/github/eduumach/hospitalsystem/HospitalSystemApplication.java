9ea98647aa3f3d854a500be30f398939e19fe5fb
Eduardo Machado Rezende <eduardomachadorezende0@gmail.com>
2022-02-02 13:50:18 -0300

    first commit

---
+++ src/main/java/com/github/eduumach/hospitalsystem/HospitalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.eduumach.hospitalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/eduumach/hospitalsystem/HospitalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.eduumach.hospitalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
